0844549925bcbd313e92e558cb85fde84f8a3721
Ahmad Bilal <ahmadbilal.se@gmail.com>
2021-11-22 12:42:11 +0700

    update: 4K resolution ui tweaks

--- pages/[country].tsx
+++ pages/[country].tsx
@@ -40,76 +40,82 @@ export default function CountryPage(props: any) {
   const detailItem = (title: string, content: string) => {
     return (
       <p>
-        <span className="font-semibold">{title}</span>: {content ? content : '-'}
+        <span className="font-semibold">{title}</span>:{" "}
+        {content ? content : "-"}
       </p>
     );
   };
 
   return (
     <PageContainer>
-      <div className="p-8 md:px-20 md:py-15">
-        <button
-          className="px-8 py-2 my-4 flex items-center bg-light-primary dark:bg-primary rounded-md drop-shadow"
-          onClick={handleBack}
-        >
-          <BiArrowBack className="mr-2" /> Back
-        </button>
-        {countryDetail ? (
-          <div className="grid gap-8 sm:grid-cols-2 sm:gap-16 items-center">
-            <div className="relative h-auto max-w-lg mt-8 self-start">
-              <Image
-                src={countryDetail.flag}
-                width={1200}
-                height={900}
-                layout="responsive"
-                objectFit="contain"
-                alt="flag"
-              />
-            </div>
-            <div className="grid grid-flow-row items-start gap-4 leading-loose lg:grid-cols-2 lg:grid-rows-auto-2">
-              <p className="text-xl font-bold lg:col-span-2 md:text-2xl lg:text-3xl">
-                {countryDetail.name}
-              </p>
-              <div>
-                {detailItem("Native name", countryDetail.nativeName)}
-                {detailItem(
-                  "Population",
-                  numberWithCommas(countryDetail.population)
-                )}
-                {detailItem("Region", countryDetail.region)}
-                {detailItem("Sub Region", countryDetail.subregion)}
-                {detailItem("Capital", countryDetail.capital)}
+      <div className="p-8 md:px-20 md:py-15 flex justify-center">
+        <div className="max-w-7xl">
+          <button
+            className="px-8 py-2 my-4 flex items-center bg-light-primary dark:bg-primary rounded-md drop-shadow"
+            onClick={handleBack}
+          >
+            <BiArrowBack className="mr-2" /> Back
+          </button>
+          {countryDetail ? (
+            <div className="grid gap-8 sm:grid-cols-2 sm:gap-16 items-center">
+              <div className="relative h-auto max-w-lg mt-8 self-start">
+                <Image
+                  src={countryDetail.flag}
+                  width={1200}
+                  height={900}
+                  layout="responsive"
+                  objectFit="contain"
+                  alt="flag"
+                />
               </div>
-              <div>
-                {detailItem(
-                  "Top Level Domain",
-                  countryDetail.topLevelDomain.join(", ")
-                )}
-                {detailItem("Currencies", countryDetail.currencies.join(" "))}
-                {detailItem("Languages", countryDetail.languages.join(" "))}
-              </div>
-              <div className="lg:col-span-2">
-                <p className="text-lg font-semibold my-4 flex flex-wrap">Border Countries: {countryDetail.borders.length === 0 ? '-' : null}</p>
-                <div className="">
-                  {countryDetail.borders.map((item: Border) => {
-                    return (
-                      <button
-                        className="px-8 my-2 mx-4 ml-0 bg-light-primary dark:bg-primary drop-shadow rounded-sm"
-                        key={item.id}
-                      >
-                        <Link href={`/${encodeURIComponent(item.name)}`}>
-                          {item.name}
-                        </Link>
-                      </button>
-                    );
-                  })}
+              <div className="grid grid-flow-row items-start gap-4 leading-loose lg:grid-cols-2 lg:grid-rows-auto-2">
+                <p className="text-xl font-bold lg:col-span-2 md:text-2xl lg:text-3xl">
+                  {countryDetail.name}
+                </p>
+                <div>
+                  {detailItem("Native name", countryDetail.nativeName)}
+                  {detailItem(
+                    "Population",
+                    numberWithCommas(countryDetail.population)
+                  )}
+                  {detailItem("Region", countryDetail.region)}
+                  {detailItem("Sub Region", countryDetail.subregion)}
+                  {detailItem("Capital", countryDetail.capital)}
+                </div>
+                <div>
+                  {detailItem(
+                    "Top Level Domain",
+                    countryDetail.topLevelDomain.join(", ")
+                  )}
+                  {detailItem("Currencies", countryDetail.currencies.join(" "))}
+                  {detailItem("Languages", countryDetail.languages.join(" "))}
+                </div>
+                <div className="lg:col-span-2">
+                  <p className="text-lg font-semibold my-4 flex flex-wrap">
+                    Border Countries:{" "}
+                    {countryDetail.borders.length === 0 ? "-" : null}
+                  </p>
+                  <div className="">
+                    {countryDetail.borders.map((item: Border) => {
+                      return (
+                        <button
+                          className="px-8 my-2 mx-4 ml-0 bg-light-primary dark:bg-primary drop-shadow rounded-sm"
+                          key={item.id}
+                        >
+                          <Link href={`/${encodeURIComponent(item.name)}`}>
+                            {item.name}
+                          </Link>
+                        </button>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <div>Loading...</div>
-        )}
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
       </div>
     </PageContainer>
   );
